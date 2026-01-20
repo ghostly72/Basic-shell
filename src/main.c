4199acee9df15de204ab6a261664e781f71f9eb4
Pretham G <unknownhawk804@gmail.com>
2026-01-20 20:21:11 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
-  char* cmd[100];
-  scanf("enter command: %c",cmd);
-  printf("%c: invalid command",cmd);
+  char cmd[100];
+  fgets(cmd,sizeof(cmd),stdin);
+  cmd[strcspn(cmd,"\n")]=0;
+  printf("%s: command not found\n",cmd);
 
   return 0;
 }
