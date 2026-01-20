d68522834dda1bfd68bfb2aef020f62f66facc8e
Pretham G <unknownhawk804@gmail.com>
2026-01-20 21:01:57 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,16 +8,33 @@ int main(int argc, char *argv[]) {
 
   while(1){
     printf("$ ");
-    char cmd[100];
-    fgets(cmd,sizeof(cmd),stdin);
-    cmd[strcspn(cmd,"\n")]=0;
-    if(strcmp("exit",cmd)==0)break;
-    else if(strncmp("echo",cmd,4)==0){
-      int len=strlen(cmd);
+    char str[100];
+    char *cmd[]={"type","exit","echo"};
+    fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]=0;
+    if(strcmp("exit",str)==0)break;
+
+    else if(strncmp("echo",str,4)==0){
+      int len=strlen(str);
       len-=5;
-      printf("%.*s\n",len,cmd+5);
+      printf("%.*s\n",len,str+5);
     }
-    else printf("%s: command not found\n",cmd);
+
+    else if(strncmp("type",str,4)==0){
+      int found=0;
+      char* cmd_name=str+5;
+      for(int i=0;i<3;i++){
+        if(strcmp(cmd_name,cmd[i])==0){
+          printf("%s is a shell builtin\n",cmd_name);
+          found=1;break;
+        }
+      }
+      if(!found){
+        printf("%s: not found\n",cmd_name);
+      }
+    }
+
+    else printf("%s: command not found\n",str);
   }
   
 
